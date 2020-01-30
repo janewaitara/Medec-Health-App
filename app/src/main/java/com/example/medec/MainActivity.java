f12db0310cd09b80629f05d4f429ce84ae1f9931
janewaitara <janewaitara99@gmail.com>
2020-01-31 01:17:19 +0300

    implemented Firebase Authentication using fireBase UI for facebook, google and phone.

--- app/src/main/java/com/example/medec/MainActivity.java
+++ app/src/main/java/com/example/medec/MainActivity.java
@@ -1,14 +1,73 @@
 package com.example.medec;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static FirebaseAuth mFirebaseAuth;
+    public static FirebaseAuth.AuthStateListener mAuthListener;
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                if (firebaseAuth.getCurrentUser() == null) {
+                   signIn();
+                }
+
+            }
+        };
+
+        attachListener();
+
     }
+    private void signIn() {
+// Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build());
+
+// Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        detachListener();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    public static void attachListener() {
+        mFirebaseAuth.addAuthStateListener(mAuthListener); }
+    public static void detachListener() {
+        mFirebaseAuth.removeAuthStateListener(mAuthListener); }
 }
