d40ceaaa2ef7cd0fcb7f80768649e103779e4d91
janewaitara <janewaitara99@gmail.com>
2020-02-04 13:47:35 +0300

    received the result in onActivityResult and customized the default firebaseUI

--- app/src/main/java/com/example/medec/MainActivity.java
+++ app/src/main/java/com/example/medec/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.medec;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 import java.util.List;
@@ -66,10 +71,9 @@ public class MainActivity extends AppCompatActivity {
     private void signIn() {
 // Choose authentication providers
         List<AuthUI.IdpConfig> providers = Arrays.asList(
-
                 new AuthUI.IdpConfig.PhoneBuilder().build(),
                 new AuthUI.IdpConfig.GoogleBuilder().build(),
-                new AuthUI.IdpConfig.FacebookBuilder().setPermissions(Arrays.asList("default", "email")).build());
+                new AuthUI.IdpConfig.FacebookBuilder().setPermissions(Arrays.asList("default", "email")).build()); //.setPermissions(Arrays.asList("default", "email")) solves the provider error
 
 
 
@@ -78,8 +82,32 @@ public class MainActivity extends AppCompatActivity {
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
                         .setAvailableProviders(providers)
+                        .setTheme(R.style.MyTheme)
                         .build(),
                 RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                //show email on Toast
+                Toast.makeText(this, ""+ user.getEmail(), Toast.LENGTH_SHORT).show();
+                loginButton.setEnabled(true);
+
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                Toast.makeText(this, ""+ response.getError(), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
