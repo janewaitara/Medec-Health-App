22240d2516d1871bbc9146827ea2390ce8d603ca
janewaitara <janewaitara99@gmail.com>
2020-05-15 07:16:26 +0300

    Modified the user to validate email and password credentials

--- app/src/main/java/com/example/medec/RegisterActivity.java
+++ app/src/main/java/com/example/medec/RegisterActivity.java
@@ -8,23 +8,35 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import org.w3c.dom.Text;
 
-public class RegisterActivity extends AppCompatActivity {
+import static com.example.medec.LoginActivity.PASSWORDLENGTH;
+
+public class RegisterActivity extends AppCompatActivity  implements AdapterView.OnItemSelectedListener {
 
     public FirebaseAuth mFirebaseAuth;
+    private TextInputLayout registerNameTextInputLayout;
+    private TextInputLayout registerEmailTextInputLayout;
+    private TextInputLayout registerPasswordTextInputLayout;
+    private TextInputLayout registerPhoneTextInputLayout;
+
     EditText fullName, registerEmail, registerPassword, registerPhone;
+    Spinner userRolesSpinner;
     Button registerButton;
     TextView registeredLogin;
     ProgressBar registerProgressBar;
@@ -34,73 +46,126 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+
+        registerNameTextInputLayout = findViewById(R.id.register_nameWrapper);
+        registerEmailTextInputLayout = findViewById(R.id.register_emailWrapper);
+        registerPasswordTextInputLayout = findViewById(R.id.register_passwordWrapper);
+        registerPhoneTextInputLayout = findViewById(R.id.register_phoneWrapper);
+
         fullName = (EditText) findViewById(R.id.register_fullname);
         registerEmail = (EditText) findViewById(R.id.register_email);
         registerPassword = (EditText) findViewById(R.id.register_password);
         registerPhone = (EditText) findViewById(R.id.register_phone);
         registerButton = (Button) findViewById(R.id.btn_register);
         registeredLogin = (TextView) findViewById(R.id.registered_login);
+        registerProgressBar = findViewById(R.id.register_progress_bar);
+
+        userRolesSpinner = findViewById(R.id.register_UserRoles_spinner);
+        ArrayAdapter<CharSequence> userRolesSpinnerAdapter = ArrayAdapter.
+                createFromResource(this,R.array.userRoles,android.R.layout.simple_spinner_item);
+        userRolesSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        userRolesSpinner.setAdapter(userRolesSpinnerAdapter);
+        userRolesSpinner.setOnItemSelectedListener(this);
 
 
         mFirebaseAuth = FirebaseAuth.getInstance();
-        registerProgressBar = findViewById(R.id.register_progress_bar);
 
 
-        if (mFirebaseAuth.getCurrentUser() == null) {
+        /*if (mFirebaseAuth.getCurrentUser() == null) {
             startActivity(new Intent(getApplicationContext(), MainActivity.class));
             finish();
         }
+*/
+    }
+
+    public boolean validatePassword(){
+        String loginPassword = registerPassword.getText().toString().trim();
+
+        if (loginPassword.isEmpty()){
+            registerPasswordTextInputLayout.setError("Password can not be empty");
+            return false;
+        }else if (loginPassword.length()< PASSWORDLENGTH){
+            registerPasswordTextInputLayout.setError("password should be greater than 6");
+            return false;
+        }else {
+            registerPasswordTextInputLayout.setError(null);
+            // loginEmailTextInputLayout.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    public boolean validateEmail(){
+        String loginEmail = registerEmail.getText().toString().trim();
+
+        if (loginEmail.isEmpty()){
+            registerEmailTextInputLayout.setError("Email address can not be empty");
+            return false;
+        }else {
+            registerEmailTextInputLayout.setError(null);
+            // loginEmailTextInputLayout.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    public void registerUser(View view) {
+        String mFullName = fullName.getText().toString();
+        String mRegisterEmail = registerEmail.getText().toString();
+        String mRegisterPassword = registerPassword.getText().toString();
+        String mRegisterPhone = registerPhone.getText().toString();
 
 
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String mFullName = fullName.getText().toString();
-                String mRegisterEmail = registerEmail.getText().toString();
-                String mRegisterPassword = registerPassword.getText().toString();
-                String mRegisterPhone = registerPhone.getText().toString();
+        /*if (TextUtils.isEmpty(mRegisterEmail)) {
+            registerEmail.setError("Email is Required");
+            return;
+        }
+        if (TextUtils.isEmpty(mRegisterPassword)) {
+            registerPassword.setError("Email is Required");
+            return;
+        }
+        if (mRegisterPassword.length() < 6) {
+            registerPassword.setError("Password must >= 6 characters");
+            return;
+        }*/
+
+        if(!validateEmail()|!validatePassword()){
+            return;
+        }
+
+        registerProgressBar.setVisibility(View.VISIBLE);
+
+        //register user
+
+        mFirebaseAuth.createUserWithEmailAndPassword(mRegisterEmail, mRegisterPassword)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            Toast.makeText(RegisterActivity.this, "User Created Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(RegisterActivity.this, "Authentication failed." + task.getException().getMessage(),
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
 
 
-                if (TextUtils.isEmpty(mRegisterEmail)) {
-                    registerEmail.setError("Email is Required");
-                    return;
-                }
-                if (TextUtils.isEmpty(mRegisterPassword)) {
-                    registerPassword.setError("Email is Required");
-                    return;
-                }
-                if (mRegisterPassword.length() < 6) {
-                    registerPassword.setError("Password must >= 6 characters");
-                    return;
-                }
+    }
 
-                registerProgressBar.setVisibility(View.VISIBLE);
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-                //register user
+    }
 
-                mFirebaseAuth.createUserWithEmailAndPassword(mRegisterEmail, mRegisterPassword)
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
-                                    Toast.makeText(RegisterActivity.this, "User Created Successfully", Toast.LENGTH_SHORT).show();
-                                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                                } else {
-                                    // If sign in fails, display a message to the user.
-
-                                    Toast.makeText(RegisterActivity.this, "Authentication failed." + task.getException().getMessage(),
-                                            Toast.LENGTH_SHORT).show();
-
-                                }
-
-                                // ...
-                            }
-                        });
-
-
-            }
-        });
     }
 }
