47c98e92f6d3785b1369e5fe1792622f2db0330a
janewaitara <janewaitara99@gmail.com>
2020-05-25 19:52:43 +0300

    created a method that reads data from a json file

--- app/src/main/java/com/example/medec/LocationActivity.java
+++ app/src/main/java/com/example/medec/LocationActivity.java
@@ -7,11 +7,21 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.SearchView;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LocationActivity extends AppCompatActivity {
 
     SearchView mSearchView;
     RecyclerView mCountyRecycler;
+
+    private List<Object> mRecyclerViewItems ;
 
 
     @Override
@@ -20,6 +30,7 @@ public class LocationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_location);
 
         mSearchView = findViewById(R.id.searchView);
+        mRecyclerViewItems = new ArrayList<>();
 
         initializeDisplayContent();
 
@@ -31,5 +42,36 @@ public class LocationActivity extends AppCompatActivity {
 
         LinearLayoutManager countiesLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false);
         mCountyRecycler.setLayoutManager(countiesLayoutManager);
+
+        LocationRecyclerAdapter locationRecyclerAdapter = new LocationRecyclerAdapter(this);
+        mCountyRecycler.setAdapter(locationRecyclerAdapter);
+
+
     }
+
+    public String readJsonDataFromFile() throws IOException{
+
+        InputStream inputStream =null;
+        StringBuilder builder = new StringBuilder();
+
+        try {
+
+            String jsonDataString = null;
+            inputStream = getResources().openRawResource(R.raw.counties);
+            BufferedReader bufferedReader = new BufferedReader
+                    (new InputStreamReader(inputStream, "UTF-8"));
+            while((jsonDataString = bufferedReader.readLine()) != null){
+                builder.append(jsonDataString);
+            }
+        }finally {
+            if (inputStream != null){
+                inputStream.close();
+            }
+
+        }
+
+        return new String(builder);
+
+    }
+
 }
